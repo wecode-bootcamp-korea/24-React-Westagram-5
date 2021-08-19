5e50cb777841952ff88796422440b37ecc6994c7
mai <mai@maiui-MacBookAir.local>
2021-08-19 20:46:22 +0900

    Add: initial settings complete

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import LoginKeum from './pages/bobaekeum/Login/LoginKeum';
-import LoginKang from './pages/yeonokkang/Login/LoginKang';
-import LoginJeong from './pages/haengunjeong/Login/LoginJeong';
 import MainKeum from './pages/bobaekeum/Main/MainKeum';
+
+import LoginKang from './pages/yeonokkang/Login/LoginKang';
 import MainKang from './pages/yeonokkang/Main/MainKang';
+
+import LoginJeong from './pages/haengunjeong/Login/LoginJeong';
 import MainJeong from './pages/haengunjeong/Main/MainJeong';
 
 class Routes extends React.Component {
@@ -14,10 +16,10 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/login-keum" component={LoginKeum} />
-          <Route exact path="/login-kang" component={LoginKang} />
-          <Route exact path="/login-jeong" component={LoginJeong} />
           <Route exact path="/main-keum" component={MainKeum} />
+          <Route exact path="/login-kang" component={LoginKang} />
           <Route exact path="/main-kang" component={MainKang} />
+          <Route exact path="/login-jeong" component={LoginJeong} />
           <Route exact path="/main-jeong" component={MainJeong} />
         </Switch>
       </Router>
